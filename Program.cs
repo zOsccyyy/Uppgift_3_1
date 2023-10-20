f9703bf5924239de55663c7d950600ed21c8212d
Admin <Admin@TEB123009>
2023-10-20 15:31:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Uppgft_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder <= 15)
+            {
+                Console.WriteLine("Du är för ung");
+            }
+            else if (ålder >= 20)
+            {
+                Console.WriteLine("Du är för gammal");
+
+            }
+            else
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+
+   
+    
+             
+            
+             
+        
+
+        }
+    }
+}
+
+
+    
+
+
+
+
+
+
+
